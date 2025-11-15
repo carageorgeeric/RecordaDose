289359bde939b04d5db111367ed17c15b5492cc3
Eric Carageorge <carageorgeeric@gmail.com>
2025-11-15 18:25:00 -0300

    Pasta com fontes adicionada e Botões Iniciais criados

--- backup.py
+++ backup.py
@@ -1,63 +1,9 @@
-from tkinter import *
-
-MatrizRemedio = []
-
-print("Seja Bem Vindo ao RecordaDose! ")
- 
-while True: 
-    remedios = input("Digite o nome do remédio: ")
-    if remedios == "":
-        print("Entrada interrompida pelo usuário. ") 
-        break 
-
-    horario = input("Digite o horário(Exemplo: Meio dia e meia " \
-    " ou 12:30, ou 11 da manhã... ): ")
-
-    MatrizRemedio.append([remedios, horario])
-
-print("\n Remédio Cadastrados: ") 
-print(f"Os seus remedios são: {MatrizRemedio}")
-
-
-
-#Bibliotecas e itens a serem adicionados
-#Tkinter
-#plyer
-
-#interfaceSimples 
-#campos de texto para horario (formato 24h: 14:30 )
-#botao de adicionar 
-#area de exibição -> Lista dos Remedios com Horarios 
-
-#noticação -> plyer
-
-
-
-# Opções  
-
-# 1 - Registrar remedio 
-# 2 - Consultar Remédios 
-# 3 - Deletar um Remédio 
-
-
-# - permitir o registro dos remedios ( input ) 
-# - permitir o registro do horario que cada remedio deve ser tomado (input) 
-# - salvar os horarios e registros de cada remedio respectivamente (lista(eu acho))
-# - notificação do remedio (duas vezes) (permitir que a pessoa escolha o intervalo de 
-# tempo entre a primeira e a segunda) 
-# - interface visual 
-
-
-# 1 - Print (registre o seu remedio) Input (nome e horario) -> Pergunta qual o intervalo 
-# de tempo para a segunda a notificação -> Salvar remedio. (resumido) 
-	
-# 2 - Mostrar a lista dos remedios (nome + horario dos dois alarmes)
-
-# 3 - Mostrar lista e permitir a remoção do remédio 
+# from tkinter import *
 
 # MatrizRemedio = []
 
 # print("Seja Bem Vindo ao RecordaDose! ")
+ 
 # while True: 
 #     remedios = input("Digite o nome do remédio: ")
 #     if remedios == "":
@@ -74,37 +20,91 @@ print(f"Os seus remedios são: {MatrizRemedio}")
 
 
 
-#Bibliotecas e itens a serem adicionados
-#Tkinter (OK)
-#plyer
+# #Bibliotecas e itens a serem adicionados
+# #Tkinter
+# #plyer
 
-#interfaceSimples 
-#campos de texto para horario (formato 24h: 14:30 )
-#botao de adicionar 
-#area de exibição -> Lista dos Remedios com Horarios 
+# #interfaceSimples 
+# #campos de texto para horario (formato 24h: 14:30 )
+# #botao de adicionar 
+# #area de exibição -> Lista dos Remedios com Horarios 
 
-#noticação -> plyer
-
-
-
-# Opções  
-
-# 1 - Registrar remedio 
-# 2 - Consultar Remédios 
-# 3 - Deletar um Remédio 
+# #noticação -> plyer
 
 
-# - permitir o registro dos remedios ( input ) 
-# - permitir o registro do horario que cada remedio deve ser tomado (input) 
-# - salvar os horarios e registros de cada remedio respectivamente (lista(eu acho))
-# - notificação do remedio (duas vezes) (permitir que a pessoa escolha o intervalo de 
-# tempo entre a primeira e a segunda) 
-# - interface visual 
+
+# # Opções  
+
+# # 1 - Registrar remedio 
+# # 2 - Consultar Remédios 
+# # 3 - Deletar um Remédio 
 
 
-# 1 - Print (registre o seu remedio) Input (nome e horario) -> Pergunta qual o intervalo 
-# de tempo para a segunda a notificação -> Salvar remedio. (resumido) 
+# # - permitir o registro dos remedios ( input ) 
+# # - permitir o registro do horario que cada remedio deve ser tomado (input) 
+# # - salvar os horarios e registros de cada remedio respectivamente (lista(eu acho))
+# # - notificação do remedio (duas vezes) (permitir que a pessoa escolha o intervalo de 
+# # tempo entre a primeira e a segunda) 
+# # - interface visual 
+
+
+# # 1 - Print (registre o seu remedio) Input (nome e horario) -> Pergunta qual o intervalo 
+# # de tempo para a segunda a notificação -> Salvar remedio. (resumido) 
 	
-# 2 - Mostrar a lista dos remedios (nome + horario dos dois alarmes)
+# # 2 - Mostrar a lista dos remedios (nome + horario dos dois alarmes)
 
-# 3 - Mostrar lista e permitir a remoção do remédio 
+# # 3 - Mostrar lista e permitir a remoção do remédio 
+
+# # MatrizRemedio = []
+
+# # print("Seja Bem Vindo ao RecordaDose! ")
+# # while True: 
+# #     remedios = input("Digite o nome do remédio: ")
+# #     if remedios == "":
+# #         print("Entrada interrompida pelo usuário. ") 
+# #         break 
+
+# #     horario = input("Digite o horário(Exemplo: Meio dia e meia " \
+# #     " ou 12:30, ou 11 da manhã... ): ")
+
+# #     MatrizRemedio.append([remedios, horario])
+
+# # print("\n Remédio Cadastrados: ") 
+# # print(f"Os seus remedios são: {MatrizRemedio}")
+
+
+
+# #Bibliotecas e itens a serem adicionados
+# #Tkinter (OK)
+# #plyer
+
+# #interfaceSimples 
+# #campos de texto para horario (formato 24h: 14:30 )
+# #botao de adicionar 
+# #area de exibição -> Lista dos Remedios com Horarios 
+
+# #noticação -> plyer
+
+
+
+# # Opções  
+
+# # 1 - Registrar remedio 
+# # 2 - Consultar Remédios 
+# # 3 - Deletar um Remédio 
+
+
+# # - permitir o registro dos remedios ( input ) 
+# # - permitir o registro do horario que cada remedio deve ser tomado (input) 
+# # - salvar os horarios e registros de cada remedio respectivamente (lista(eu acho))
+# # - notificação do remedio (duas vezes) (permitir que a pessoa escolha o intervalo de 
+# # tempo entre a primeira e a segunda) 
+# # - interface visual 
+
+
+# # 1 - Print (registre o seu remedio) Input (nome e horario) -> Pergunta qual o intervalo 
+# # de tempo para a segunda a notificação -> Salvar remedio. (resumido) 
+	
+# # 2 - Mostrar a lista dos remedios (nome + horario dos dois alarmes)
+
+# # 3 - Mostrar lista e permitir a remoção do remédio 
